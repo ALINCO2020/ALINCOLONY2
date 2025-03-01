839fde5405b7f9bd1b358d371e5582cbea7f77dd
alinco <alinco2020xxx@gmail.com>
2025-03-01 19:11:27 +0900

    デザイン調整

--- src/components/UI/PaginationPrevNext.svelte
+++ src/components/UI/PaginationPrevNext.svelte
@@ -5,11 +5,11 @@
 
 {#if type === "prev"}
     <a {href} aria-label="previous page" class="pagenation-a">
-        <span class="duration-150 group-hover:text-teal-500"> Prev </span>
+        <span class="duration-150 group-hover:text-teal-500">&lt;</span>
     </a>
 {:else if type === "next"}
     <a {href} aria-label="next page" class="pagenation-a">
-        <span class="duration-150 group-hover:text-teal-500"> Next </span>
+        <span class="duration-150 group-hover:text-teal-500">&gt;</span>
     </a>
 {/if}
 
